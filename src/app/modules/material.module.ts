584907d7a14a356ed4857c963ebce9c65d138980
hellochrisyou <hellochrisyou@gmail.com>
2020-05-04 03:34:56 -0400

    6 files modified

--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -9,19 +9,26 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatStepperModule } from '@angular/material/stepper';
 import {MatTableModule} from '@angular/material/table';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatExpansionModule} from '@angular/material/expansion';
+import { MatTooltipModule } from '@angular/material/tooltip';
+    
 
 @NgModule({
   exports: [
     DragDropModule,
+    MatBadgeModule,
     MatStepperModule,
     MatInputModule,
     MatIconModule,
     MatSelectModule,
     MatCheckboxModule,
     MatSnackBarModule,
+    MatTooltipModule,
     MatButtonModule,
     MatTableModule,
     FlexLayoutModule,
+    MatExpansionModule
   ]
 })
 export class MaterialModule {}
